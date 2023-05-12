0f477468ccee3f3e2db71fe708455e46948aad29
Louis <louis.mueller@axonivy.com>
2023-05-12 13:33:20 +0200

    XIVY-11010 fix raise sed command

--- .ivy/raise-version.sh
+++ .ivy/raise-version.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+
 yarn install --ignore-scripts
 yarn lerna version ${1/-SNAPSHOT/} --no-git-tag-version --no-push --ignore-scripts --exact --yes
-sed -i -E "s/(\"@axonivy[^\"]*\"): \"[^\"]*\"/\1: \"~${1/SNAPSHOT/next}\"/" vscode-extensions/*/*/package.json
+sed -i '' "s/\(\"@axonivy[^\"]*\"\): \"[^\"]*\"/\1: \"~${1}-next\"/" vscode-extensions/*/*/package.json
 yarn update:axonivy:next
 yarn install --ignore-scripts
