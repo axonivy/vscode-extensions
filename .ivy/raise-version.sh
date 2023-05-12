983da559d0aa778da44a6008985c94c590db6e37
Louis <louis.mueller@axonivy.com>
2023-05-12 11:37:26 +0200

    XIVY-11010 make raise script executable

--- .ivy/raise-version.sh
+++ .ivy/raise-version.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 yarn install --ignore-scripts
 yarn lerna version ${1/-SNAPSHOT/} --no-git-tag-version --no-push --ignore-scripts --exact --yes
 sed -i -E "s/(\"@axonivy[^\"]*\"): \"[^\"]*\"/\1: \"~${1/SNAPSHOT/next}\"/" vscode-extensions/*/*/package.json
