4f85df2b24a329dc74be1e749371b5d8bcfa6344
Alexander Suter <alexander.suter@axonivy.com>
2023-05-19 14:03:45 +0200

    Split raise version and raise deps

--- .ivy/raise-version.sh
+++ .ivy/raise-version.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 yarn install --ignore-scripts
 yarn lerna version ${1/-SNAPSHOT/} --no-git-tag-version --no-push --ignore-scripts --exact --yes
-sed -i -E "s/(\"@axonivy[^\"]*\"): \"[^\"]*\"/\1: \"~${1/SNAPSHOT/next}\"/" vscode-extensions/*/*/package.json
-yarn update:axonivy:next
-if [$2 -eq 0]
-then
-  yarn install --ignore-scripts
-fi
