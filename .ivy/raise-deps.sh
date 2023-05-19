3aeec2a292eb887a07f4b538d8529793069bf426
Alexander Suter <alexander.suter@axonivy.com>
2023-05-19 14:16:37 +0200

    Move yarn install --ignore-scripts to the end

--- .ivy/raise-deps.sh
+++ .ivy/raise-deps.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-yarn install --ignore-scripts
 sed -i -E "s/(\"@axonivy[^\"]*\"): \"[^\"]*\"/\1: \"~${1/SNAPSHOT/next}\"/" vscode-extensions/*/*/package.json
 yarn update:axonivy:next
+yarn install --ignore-scripts
