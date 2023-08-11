a3c937a5cedd585647ededa6d2da84c8e7fa4016
Louis <louis.mueller@axonivy.com>
2023-08-11 13:58:45 +0200

    XIVY-12086 trigger engine if there is a ivy project

--- vscode-extensions/project-explorer/extension/src/ivy-project-explorer.ts
+++ vscode-extensions/project-explorer/extension/src/ivy-project-explorer.ts
@@ -7,10 +7,21 @@ export class IvyProjectExplorer {
   constructor(context: vscode.ExtensionContext) {
     const treeDataProvider = new IvyProjectTreeDataProvider();
     context.subscriptions.push(vscode.window.createTreeView(VIEW_ID, { treeDataProvider }));
-    treeDataProvider.hasIvyProjcts().then(hasIvyProjcts => this.setHasIvyProjectsToContext(hasIvyProjcts));
+    treeDataProvider.hasIvyProjcts().then(hasIvyProjcts => this.notify(hasIvyProjcts));
+  }
+
+  notify(hasIvyProjcts: boolean) {
+    this.setHasIvyProjectsToContext(hasIvyProjcts);
+    this.activateEngineExtension(hasIvyProjcts);
   }
 
   setHasIvyProjectsToContext(hasIvyProjcts: boolean) {
     vscode.commands.executeCommand('setContext', 'ivy:hasIvyProjects', hasIvyProjcts);
+  }
+
+  activateEngineExtension(hasIvyProjcts: boolean) {
+    if (hasIvyProjcts) {
+      vscode.commands.executeCommand('activateIvyEngine');
+    }
   }
 }
