8c579a799c1bdd2cd30232dd9d70ebb0a73ed4a3
Louis <louis.mueller@axonivy.com>
2023-08-17 14:43:10 +0200

    XIVY-12086 fix extension activation chain for theia (#63)

--- vscode-extensions/base/src/index.ts
+++ vscode-extensions/base/src/index.ts
@@ -1,18 +1,2 @@
-export const PROCESS_EDITOR_EXTENSION_ID = 'axonivy.vscode-process-editor-extension';
-
-export type ElementId = {
-  app: string;
-  pmv: string;
-  pid: string;
-};
-
-export type SelectedElement = ElementId | undefined;
-
-export interface ProcessEditorConnector {
-  getSelectedElement(): SelectedElement;
-  onSelectedElement(listener: (selectedElement: SelectedElement) => any): void;
-}
-
-export interface ProcessEditorExtension {
-  connector: ProcessEditorConnector;
-}
+export * from './process-editor-connector';
+export * from './commands';
