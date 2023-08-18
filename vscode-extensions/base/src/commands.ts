68155364865fc525309f081c166c8f8da333c7c3
Louis <louis.mueller@axonivy.com>
2023-08-18 13:55:38 +0200

    extension activation improvement

--- vscode-extensions/base/src/commands.ts
+++ vscode-extensions/base/src/commands.ts
@@ -4,7 +4,6 @@ export namespace Commands {
   export const VSCODE_SET_CONTEXT = 'setContext';
   export const ENGINE_RESOLVE_URL = 'vscode-engine-extension.resolveEngineUrl';
   export const PROJECT_EXPLORER_HAS_IVY_PROJECTS = 'vscode-project-explorer-extension.hasIvyProjects';
-  export const PROCESS_EDITOR_ACTIVATE = 'vscode-process-editor-extension.activate';
 }
 
 export async function executeCommand(commandName: string, ...rest: any[]) {
