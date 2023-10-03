a67270f9c2a7fe4449bb542c7a858fa3c1943461
Louis <louis.mueller@axonivy.com>
2023-10-03 12:37:04 +0200

    XIVY-12484 keep project explorer in sync with active tab

--- vscode-extensions/project-explorer/extension/src/ivy-project-explorer.ts
+++ vscode-extensions/project-explorer/extension/src/ivy-project-explorer.ts
@@ -6,20 +6,31 @@ export const VIEW_ID = 'ivyProjects';
 
 export class IvyProjectExplorer {
   private treeDataProvider: IvyProjectTreeDataProvider;
+  private treeView: vscode.TreeView<Entry>;
 
   constructor(context: vscode.ExtensionContext) {
     this.treeDataProvider = new IvyProjectTreeDataProvider();
-    context.subscriptions.push(vscode.window.createTreeView(VIEW_ID, { treeDataProvider: this.treeDataProvider, showCollapseAll: true }));
+    this.treeView = vscode.window.createTreeView(VIEW_ID, { treeDataProvider: this.treeDataProvider, showCollapseAll: true });
+    context.subscriptions.push(this.treeView);
+    this.registerCommands();
+    this.defineIvyProjectFileWatcher();
+    vscode.window.tabGroups.onDidChangeTabs(async event => this.changeTabListener(event));
+    this.hasIvyProjects().then(hasIvyProjects => this.setProjectExplorerActivationCondition(hasIvyProjects));
+  }
+
+  private registerCommands(): void {
     vscode.commands.registerCommand(`${VIEW_ID}.refreshEntry`, () => this.refresh());
     vscode.commands.registerCommand(`${VIEW_ID}.buildAndDeployAll`, () => this.buildAndDeployAll());
     vscode.commands.registerCommand(`${VIEW_ID}.buildAndDeployProject`, (entry: Entry) => this.buildAndDeployProject(entry));
     vscode.commands.registerCommand(Commands.PROJECT_EXPLORER_HAS_IVY_PROJECTS, () => this.hasIvyProjects());
     vscode.commands.registerCommand(Commands.PROJECT_EXPLORER_GET_IVY_PROJECTS, () => this.treeDataProvider.getIvyProjects());
+  }
+
+  private defineIvyProjectFileWatcher(): void {
     const projectFileWatcher = vscode.workspace.createFileSystemWatcher(IVY_RPOJECT_FILE_PATTERN);
     projectFileWatcher.onDidChange(() => this.refresh());
     projectFileWatcher.onDidCreate(() => this.refresh());
     projectFileWatcher.onDidDelete(() => this.refresh());
-    this.hasIvyProjects().then(hasIvyProjects => this.setProjectExplorerActivationCondition(hasIvyProjects));
   }
 
   private async hasIvyProjects(): Promise<boolean> {
@@ -48,6 +59,23 @@ export class IvyProjectExplorer {
   private activateEngineExtension(hasIvyProjects: boolean): void {
     if (hasIvyProjects) {
       executeCommand(Commands.ENGINE_START_MANAGER);
+    }
+  }
+
+  private changeTabListener(event: vscode.TabChangeEvent): void {
+    if (!event.changed) {
+      return;
+    }
+    this.syncProjectExplorerSelectionWithActiveTab();
+  }
+
+  private syncProjectExplorerSelectionWithActiveTab(): void {
+    const tabInput = vscode.window.tabGroups.activeTabGroup.activeTab?.input as { uri: vscode.Uri };
+    if (tabInput && tabInput.uri) {
+      const entry = this.treeDataProvider.getEntryCache().get(tabInput.uri.fsPath);
+      if (entry && this.treeView.visible) {
+        this.treeView.reveal(entry);
+      }
     }
   }
 }
