170f362946ded2b846bdba67280228301562b733
Louis <louis.mueller@axonivy.com>
2023-10-06 11:31:40 +0200

    w

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-Xvfb :99 -screen 0 1360x1020x24 -dpi 24 -listen tcp -noreset -ac +extension RANDR & exec "$@"
+# Xvfb :99 -screen 0 1360x1020x24 -dpi 24 -listen tcp -noreset -ac +extension RANDR & exec "$@"
+
+echo "Starting Xvfb"
+Xvfb :99 -ac &
+sleep 2
+
+export DISPLAY=:99
+echo "Executing command $@"
+
+exec "$@"
