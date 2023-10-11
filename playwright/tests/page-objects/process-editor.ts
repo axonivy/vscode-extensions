2c95b1b3672fe021bb5b61aaa2fc0a54c8852423
Louis <louis.mueller@axonivy.com>
2023-10-11 16:34:11 +0200

    wip

--- playwright/tests/page-objects/process-editor.ts
+++ playwright/tests/page-objects/process-editor.ts
@@ -1,13 +1,14 @@
-import { FrameLocator, Locator, Page } from 'playwright-core';
+import { Locator, Page } from 'playwright-core';
 import { View, ViewData } from './view';
 import { getCtrlOrMeta } from '../utils/keyboard';
 import { expect } from 'playwright/test';
+import { FrameLocator } from '@playwright/test';
 
 export class ProcessEditor extends View {
   constructor(page: Page, private filePath: string = 'ProcurementRequestUserTask.p.json') {
     const outputViewData: ViewData = {
       tabSelector: `div.tab:has-text("${filePath}")`,
-      viewSelector: 'div'
+      viewSelector: 'div.editor-instance > div'
     };
     super(outputViewData, page);
   }
@@ -20,16 +21,20 @@ export class ProcessEditor extends View {
 
   override async isViewVisible(): Promise<void> {
     await this.isTabVisible();
-    const graph = this.viewFrameLoactor().locator('.sprotty-graph');
+    const graph = (await this.viewFrameLoactor()).locator('.sprotty-graph');
     await expect(graph).toBeVisible();
   }
 
-  protected viewFrameLoactor(): FrameLocator {
-    return this.viewLocator.frameLocator('iFrame').frameLocator('iFrame#active-frame');
+  async viewFrameLoactor(): Promise<FrameLocator> {
+    await expect(this.page.locator('.monaco-breadcrumbs')).toContainText(this.filePath);
+    await expect(this.viewLocator).toBeEnabled();
+    await expect(this.viewLocator).toHaveAttribute('aria-flowto', /.*/);
+    const flowtoId = await this.viewLocator.getAttribute('aria-flowto');
+    return this.page.locator(`[id='${flowtoId}']`).frameLocator('iFrame').frameLocator('iFrame#active-frame');
   }
 
-  locatorForPID(pid: string): Locator {
-    return this.viewFrameLoactor().locator(`[id$="_${pid}"]`);
+  async locatorForPID(pid: string): Promise<Locator> {
+    return (await this.viewFrameLoactor()).locator(`[id$="_${pid}"]`);
   }
 
   async typeText(text: string): Promise<void> {
