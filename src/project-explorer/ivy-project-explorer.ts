b39b25736c10af5af35dfb2fb9aef588a8d2c750
Louis <louis.mueller@axonivy.com>
2023-11-03 15:06:16 +0100

    XIVY-11752 add ivy activity bar & filter hidden files & cleanup

--- src/project-explorer/ivy-project-explorer.ts
+++ src/project-explorer/ivy-project-explorer.ts
@@ -23,7 +23,10 @@ export class IvyProjectExplorer {
       this.revealActiveEntry(entry);
     });
 
-    this.hasIvyProjects().then(hasIvyProjects => this.setProjectExplorerActivationCondition(hasIvyProjects));
+    this.hasIvyProjects().then(hasIvyProjects => {
+      this.setProjectExplorerActivationCondition(hasIvyProjects);
+      this.focusIvyView(hasIvyProjects);
+    });
   }
 
   private registerCommands(): void {
@@ -96,6 +99,12 @@ export class IvyProjectExplorer {
 
   private setProjectExplorerActivationCondition(hasIvyProjects: boolean): void {
     executeCommand(Commands.VSCODE_SET_CONTEXT, 'ivy:hasIvyProjects', hasIvyProjects);
+  }
+
+  private focusIvyView(hasIvyProjects: boolean): void {
+    if (hasIvyProjects) {
+      executeCommand(`${VIEW_ID}.focus`);
+    }
   }
 
   private activateEngineExtension(hasIvyProjects: boolean): void {
--- src/project-explorer/ivy-project-tree-data-provider.ts
+++ src/project-explorer/ivy-project-tree-data-provider.ts
@@ -1,7 +1,5 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
-import * as fs from 'fs';
-import { FileStat } from './file-stat';
 
 export interface Entry {
   uri: vscode.Uri;
@@ -48,15 +46,22 @@ export class IvyProjectTreeDataProvider implements vscode.TreeDataProvider<Entry
 
   private async findIvyProjects(): Promise<string[]> {
     const ivyProjectFiles = await vscode.workspace.findFiles(IVY_RPOJECT_FILE_PATTERN);
-    return ivyProjectFiles
-      .filter(uri => this.containsIvyProjectNature(uri.fsPath))
-      .map(projectFile => path.dirname(projectFile.fsPath))
+    const ivyProjectFilesWihtInclude = await Promise.all(
+      ivyProjectFiles.map(async uri => ({
+        uri: uri,
+        include: await this.containsIvyProjectNature(uri)
+      }))
+    );
+    return ivyProjectFilesWihtInclude
+      .filter(e => e.include)
+      .map(e => path.dirname(e.uri.fsPath))
       .sort();
   }
 
-  private containsIvyProjectNature(path: string) {
-    const contents = fs.readFileSync(path, 'utf-8');
-    return contents.includes('<nature>ch.ivyteam.ivy.project.IvyProjectNature</nature>');
+  private async containsIvyProjectNature(uri: vscode.Uri): Promise<boolean> {
+    const bytes = await vscode.workspace.fs.readFile(uri);
+    const content = Buffer.from(bytes).toString('utf8');
+    return content.includes('<nature>ch.ivyteam.ivy.project.IvyProjectNature</nature>');
   }
 
   async hasIvyProjects(): Promise<boolean> {
@@ -113,7 +118,9 @@ export class IvyProjectTreeDataProvider implements vscode.TreeDataProvider<Entry
   async getChildren(element?: Entry): Promise<Entry[]> {
     if (element) {
       const children = await this.readDirectory(element.uri);
-      return children.map(([childName, childType]) => this.createAndCacheChild(element, childName, childType));
+      return children
+        .map(([childName, childType]) => this.createAndCacheChild(element, childName, childType))
+        .sort((a, b) => (a.type > b.type ? -1 : 0));
     }
     return (await this.ivyProjects).map(dir => this.createAndCacheRoot(dir));
   }
@@ -141,68 +148,7 @@ export class IvyProjectTreeDataProvider implements vscode.TreeDataProvider<Entry
   }
 
   async readDirectory(uri: vscode.Uri): Promise<[string, vscode.FileType][]> {
-    const children = await this.readdir(uri.fsPath);
-    const result: [string, vscode.FileType][] = [];
-    for (const child of children) {
-      const fileStat = await this.fileStat(path.join(uri.fsPath, child));
-      result.push([child, fileStat.type]);
-    }
-    return result;
-  }
-
-  async readdir(path: string): Promise<string[]> {
-    return new Promise<string[]>((resolve, reject) => {
-      fs.readdir(path, (error, children) => this.handleResult(resolve, reject, error, this.normalizeNFC(children)));
-    });
-  }
-
-  normalizeNFC(items: string[]): string[] {
-    if (process.platform !== 'darwin') {
-      return items;
-    }
-
-    if (Array.isArray(items)) {
-      return items.map(item => item.normalize('NFC'));
-    }
-    return [];
-  }
-
-  async fileStat(path: string): Promise<vscode.FileStat> {
-    return new FileStat(await this.fsStats(path));
-  }
-
-  async fsStats(path: string): Promise<fs.Stats> {
-    return new Promise<fs.Stats>((resolve, reject) => {
-      fs.stat(path, (error, stat) => this.handleResult(resolve, reject, error, stat));
-    });
-  }
-
-  handleResult<T>(resolve: (result: T) => void, reject: (error: Error) => void, error: Error | null | undefined, result: T): void {
-    if (error) {
-      reject(this.massageError(error));
-    } else {
-      resolve(result);
-    }
-  }
-
-  massageError(error: Error & { code?: string }): Error {
-    if (error.code === 'ENOENT') {
-      return vscode.FileSystemError.FileNotFound();
-    }
-
-    if (error.code === 'EISDIR') {
-      return vscode.FileSystemError.FileIsADirectory();
-    }
-
-    if (error.code === 'EEXIST') {
-      return vscode.FileSystemError.FileExists();
-    }
-
-    if (error.code === 'EPERM' || error.code === 'EACCES') {
-      return vscode.FileSystemError.NoPermissions();
-    }
-
-    return error;
+    return (await vscode.workspace.fs.readDirectory(uri)).filter(([childName]) => !childName.startsWith('.'));
   }
 
   static rootOf(child: Entry): Entry {
