c98dd6508559cd9702f51869d12502e7bdbcb6ec
Christian Strebel <christian.strebel@axonivy.com>
2023-11-24 12:10:53 +0100

    Change also the engineDownloadUrl on raise debs

--- .ivy/raise-deps.sh
+++ .ivy/raise-deps.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+shopt -s globstar
+# likely not working on mac
+sed -i -E "s|core_product-engine/job/master/|core_product-engine/job/release%2F${1/.[0-9]-SNAPSHOT/}/|" build/**/Jenkinsfile
 sed -i -E "s/(\"@axonivy[^\"]*\"): \"[^\"]*\"/\1: \"~${1/SNAPSHOT/next}\"/" extension/webviews/*/package.json
 yarn update:axonivy:next
 yarn install --ignore-scripts
