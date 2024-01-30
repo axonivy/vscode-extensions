597e68adfd7f428a20f5e0b361092263fe7de723
Louis <louis.mueller@axonivy.com>
2024-01-30 10:03:11 +0100

    split tests

--- playwright/tests/create-process.test.ts
+++ playwright/tests/create-process.test.ts
@@ -1,5 +1,4 @@
-import { test } from 'playwright/test';
-import { pageFor } from './fixtures/page';
+import { pageFor, test } from './fixtures/page';
 import { multiProjectWorkspacePath, removeFromWorkspace } from './workspaces/workspace';
 import { Page, expect } from '@playwright/test';
 import { ProcessEditor } from './page-objects/process-editor';
@@ -8,20 +7,21 @@ import path from 'path';
 import { wait } from './utils/timeout';
 import { randomInt } from 'crypto';
 
-test.describe.only('Create Process', () => {
+test.describe.only(() => {
   let page: Page;
   let explorer: FileExplorer;
   let processEditor: ProcessEditor;
   let processName: string;
   const projectName = 'prebuiltProject';
   const cleanUp = () => removeFromWorkspace(path.join(multiProjectWorkspacePath, projectName), 'processes');
-
-  test.beforeAll(async ({}, testInfo) => {
-    cleanUp();
-    page = await pageFor(multiProjectWorkspacePath, testInfo.titlePath[1]);
+  const setup = async () => {
     explorer = new FileExplorer(page);
     processEditor = new ProcessEditor(page);
     await processEditor.hasStatusMessage('Finished: Deploy Ivy Projects');
+  };
+
+  test.beforeAll(async () => {
+    cleanUp();
   });
 
   test.beforeEach(async () => {
@@ -37,47 +37,61 @@ test.describe.only('Create Process', () => {
     cleanUp();
   });
 
-  test('Assert that process gets redeployed after editing', async () => {
-    await explorer.addProcess(projectName, processName, 'Business Process');
-    await explorer.hasNoStatusMessage();
-    const start = processEditor.locatorForElementType('g.start\\:requestStart');
-    await processEditor.appendActivity(start, 'Script');
-    const script = processEditor.locatorForElementType('g.script');
-    await expect(script).toHaveClass(/selected/);
-    await processEditor.saveAllFiles();
-    await explorer.hasStatusMessage('Finished: Deploy Ivy Projects');
-    await processEditor.startProcessAndAssertExecuted(start, script);
+  test.describe('Create', () => {
+    test.beforeAll(async ({}, testInfo) => {
+      page = await pageFor(multiProjectWorkspacePath, testInfo.titlePath[1]);
+      await setup();
+    });
+
+    test('Add nested business process', async () => {
+      await explorer.addProcess(projectName, `parent1/parent2/${processName}`, 'Business Process');
+      await explorer.hasNode('parent1');
+      await explorer.hasNode('parent2');
+      await explorer.hasNode(`${processName}.p.json`);
+      const start = processEditor.locatorForElementType('g.start\\:requestStart');
+      await expect(start).toBeVisible();
+    });
+
+    test('Add callable sub process', async () => {
+      await explorer.addProcess(projectName, processName, 'Callable Sub Process');
+      await explorer.hasNode(`${processName}.p.json`);
+      const start = processEditor.locatorForElementType('g.start\\:callSubStart');
+      await expect(start).toBeVisible();
+    });
+
+    test('Add web service process', async () => {
+      await explorer.addProcess(projectName, processName, 'Web Service Process');
+      await explorer.hasNode(`${processName}.p.json`);
+      const start = processEditor.locatorForElementType('g.start\\:webserviceStart');
+      await expect(start).toBeVisible();
+    });
   });
 
-  test('Add business process and execute it', async () => {
-    await explorer.addProcess(projectName, processName, 'Business Process');
-    await explorer.hasNode(`${processName}.p.json`);
-    await explorer.hasStatusMessage('Finished: Deploy Ivy Projects');
-    const start = processEditor.locatorForElementType('g.start\\:requestStart');
-    const end = processEditor.locatorForElementType('g.end\\:taskEnd');
-    await processEditor.startProcessAndAssertExecuted(start, end);
-  });
+  test.describe('Create and Execute', () => {
+    test.beforeEach(async ({ pageFor }) => {
+      page = await pageFor(multiProjectWorkspacePath);
+      await setup();
+    });
 
-  test('Add nested business process', async () => {
-    await explorer.addProcess(projectName, `parent1/parent2/${processName}`, 'Business Process');
-    await explorer.hasNode('parent1');
-    await explorer.hasNode('parent2');
-    await explorer.hasNode(`${processName}.p.json`);
-    const start = processEditor.locatorForElementType('g.start\\:requestStart');
-    await expect(start).toBeVisible();
-  });
+    test('Add business process and execute it', async () => {
+      await explorer.addProcess(projectName, processName, 'Business Process');
+      await explorer.hasNode(`${processName}.p.json`);
+      await explorer.hasStatusMessage('Finished: Deploy Ivy Projects');
+      const start = processEditor.locatorForElementType('g.start\\:requestStart');
+      const end = processEditor.locatorForElementType('g.end\\:taskEnd');
+      await processEditor.startProcessAndAssertExecuted(start, end);
+    });
 
-  test('Add callable sub process', async () => {
-    await explorer.addProcess(projectName, processName, 'Callable Sub Process');
-    await explorer.hasNode(`${processName}.p.json`);
-    const start = processEditor.locatorForElementType('g.start\\:callSubStart');
-    await expect(start).toBeVisible();
-  });
-
-  test('Add web service process', async () => {
-    await explorer.addProcess(projectName, processName, 'Web Service Process');
-    await explorer.hasNode(`${processName}.p.json`);
-    const start = processEditor.locatorForElementType('g.start\\:webserviceStart');
-    await expect(start).toBeVisible();
+    test('Assert that process gets redeployed after editing', async () => {
+      await explorer.addProcess(projectName, processName, 'Business Process');
+      await explorer.hasNoStatusMessage();
+      const start = processEditor.locatorForElementType('g.start\\:requestStart');
+      await processEditor.appendActivity(start, 'Script');
+      const script = processEditor.locatorForElementType('g.script');
+      await expect(script).toHaveClass(/selected/);
+      await processEditor.saveAllFiles();
+      await explorer.hasStatusMessage('Finished: Deploy Ivy Projects');
+      await processEditor.startProcessAndAssertExecuted(start, script);
+    });
   });
 });
