8b52810a4004f044deb40598d47364dc5e0c6411
Louis <louis.mueller@axonivy.com>
2024-04-08 10:40:10 +0200

    dont record video

--- playwright/tests/utils/app.ts
+++ playwright/tests/utils/app.ts
@@ -18,9 +18,9 @@ const args = [
 export async function launchElectronApp(workspacePath: string, testTitle: string): Promise<ElectronApplication> {
   return await _electron.launch({
     executablePath: await downloadAndUnzipVSCode(downloadVersion),
-    args: [...args, workspacePath],
-    recordVideo: {
-      dir: path.join(__dirname, '..', '..', 'playwright-videos', testTitle.replaceAll(' ', '_'))
-    }
+    args: [...args, workspacePath]
+    // recordVideo: {
+    //   dir: path.join(__dirname, '..', '..', 'playwright-videos', testTitle.replaceAll(' ', '_'))
+    // }
   });
 }
