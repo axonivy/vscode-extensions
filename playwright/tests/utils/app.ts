f8006c640880930facd8c9fec03614cf99712284
Louis <louis.mueller@axonivy.com>
2024-04-08 10:19:47 +0200

    remove no sandbox

--- playwright/tests/utils/app.ts
+++ playwright/tests/utils/app.ts
@@ -9,8 +9,7 @@ const args = [
   '--extensionDevelopmentPath=' + path.resolve(__dirname, '../../../extension/'),
   '--new-window',
   '--skip-release-notes',
-  '--skip-welcome',
-  '--no-sandbox'
+  '--skip-welcome'
 ];
 
 export async function launchElectronApp(workspacePath: string, testTitle: string): Promise<ElectronApplication> {
