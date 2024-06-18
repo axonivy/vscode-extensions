8525da58ecebd663605862bf05d9ccf56d0dd837
Lukas Lieb <lukas.lieb@axonivy.com>
2024-06-18 09:49:49 +0200

    XIVY-14270 Reuse StartProcessAction from protocol

--- extension/webviews/process-editor/src/start/action.ts
+++ extension/webviews/process-editor/src/start/action.ts
@@ -1,27 +1,8 @@
 import { inject, injectable } from 'inversify';
-import { IActionHandler, GModelElement } from '@eclipse-glsp/client';
-import { EventStartTypes, QuickAction, SingleQuickActionProvider, StartEventNode } from '@axonivy/process-editor';
-import { IvyIcons } from '@axonivy/ui-icons';
+import { IActionHandler } from '@eclipse-glsp/client';
 import { HOST_EXTENSION, RequestType } from 'vscode-messenger-common';
 import { Messenger } from 'vscode-messenger-webview';
-
-export interface StartProcessAction {
-  kind: typeof StartProcessAction.KIND;
-  elementId: string;
-  processStartUri: string;
-}
-
-export namespace StartProcessAction {
-  export const KIND = 'startProcess';
-
-  export function create(elementId: string, processStartUri: string): StartProcessAction {
-    return {
-      kind: KIND,
-      elementId,
-      processStartUri
-    };
-  }
-}
+import { StartProcessAction } from '@axonivy/process-editor-protocol';
 
 const StartProcessRequest: RequestType<string, void> = { method: 'startProcess' };
 
@@ -31,25 +12,5 @@ export class StartProcessActionHandler implements IActionHandler {
 
   handle(action: StartProcessAction) {
     this.messenger.sendRequest(StartProcessRequest, HOST_EXTENSION, action.processStartUri);
-  }
-}
-
-@injectable()
-export class StarProcessQuickActionProvider extends SingleQuickActionProvider {
-  singleQuickAction(element: GModelElement): QuickAction | undefined {
-    if (element instanceof StartEventNode && element.type === EventStartTypes.START) {
-      const processStartUri = (element.args['processStartUri'] as string) ?? '';
-      return {
-        icon: IvyIcons.Play,
-        title: 'Start Process (X)',
-        location: 'Left',
-        sorting: 'A',
-        action: StartProcessAction.create(element.id, processStartUri),
-        readonlySupport: true,
-        shortcut: 'KeyX',
-        removeSelection: true
-      };
-    }
-    return undefined;
   }
 }
