cc19af25631e1ded9469d2a0ac3bb13d140decd6
Louis <louis.mueller@axonivy.com>
2024-06-24 09:11:26 +0200

    XIVY-14102 remove unnecessary build & deploy, engine does it now

--- extension/src/engine/api/api-constants.ts
+++ extension/src/engine/api/api-constants.ts
@@ -5,8 +5,6 @@ export type SourcePath =
   | 'project'
   | 'project/init'
   | 'project/deploy'
-  | 'project/deactivate'
-  | 'project/activate'
   | 'process'
   | 'hd'
   | 'project/watch'
@@ -15,8 +13,6 @@ export type Request = { path: SourcePath; description: string };
 
 export const DEV_CONTEXT: Readonly<Request> = { path: 'dev-security-context', description: 'Init dev Security Context' };
 export const INIT_PROJECT: Readonly<Request> = { path: 'project/init', description: 'Initialize Ivy Project' };
-export const DEACTIVATE_PROJECTS: Readonly<Request> = { path: 'project/deactivate', description: 'Deactivate Ivy Projects' };
-export const ACTIVATE_PROJECTS: Readonly<Request> = { path: 'project/activate', description: 'Activate Ivy Project' };
 export const DEPLOY_PROJECTS: Readonly<Request> = { path: 'project/deploy', description: 'Deploy Ivy Projects' };
 export const CREATE_PROCESS: Readonly<Request> = { path: 'process', description: 'Create new Process' };
 export const CREATE_PROJECT: Readonly<Request> = { path: 'project', description: 'Create new Project' };
