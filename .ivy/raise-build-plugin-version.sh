33d3dcf1fe6f41f459d74f6d2b1aacdd44cba4ff
Louis <louis.mueller@axonivy.com>
2024-07-25 13:11:38 +0200

    wip

--- .ivy/raise-build-plugin-version.sh
+++ .ivy/raise-build-plugin-version.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 mvn --batch-mode versions:set-property versions:commit -f playwright/tests/workspaces/prebuiltProject/pom.xml -Dproperty=project.build.plugin.version -DnewVersion=${2} -DallowSnapshots=true
+mvn --batch-mode versions:set-property versions:commit -f playwright/tests/workspaces/prebuiltEmptyProject/pom.xml -Dproperty=project.build.plugin.version -DnewVersion=${2} -DallowSnapshots=true
