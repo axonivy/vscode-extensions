38f93e8f94edad411d50587036e7a7410528855f
Louis <louis.mueller@axonivy.com>
2024-07-25 16:23:42 +0200

    wip

--- playwright/tests/process-animation.test.ts
+++ playwright/tests/process-animation.test.ts
@@ -1,16 +1,23 @@
-import { test } from './fixtures/page';
 import { ProcessEditor } from './page-objects/process-editor';
 import { prebuiltWorkspacePath } from './workspaces/workspace';
+import { pageFor } from './fixtures/page';
+import { test } from '@playwright/test';
 
 test.describe('Process Animation', () => {
-  test('Animate Call Sub', async ({ pageFor }) => {
-    const page = await pageFor(prebuiltWorkspacePath);
-    const processEditor = new ProcessEditor(page, 'Animation.p.json');
+  let processEditor: ProcessEditor;
+
+  test.beforeAll(async ({}, testInfo) => {
+    const page = await pageFor(prebuiltWorkspacePath, testInfo.titlePath[1]);
+    processEditor = new ProcessEditor(page, 'Animation.p.json');
     await processEditor.hasDeployProjectStatusMessage();
-    await processEditor.hasNoStatusMessage();
+  });
+
+  test.beforeEach(async () => {
     await processEditor.openEditorFile();
     await processEditor.isViewVisible();
+  });
 
+  test('Animate Call Sub', async () => {
     const start = processEditor.locatorForPID('190E9B4311474684-f0');
     const scriptInCallSub = processEditor.locatorForPID('190E938617AE0413-f3');
     await processEditor.startProcessAndAssertExecuted(start, scriptInCallSub);
