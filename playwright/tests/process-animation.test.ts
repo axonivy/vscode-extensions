ebff6bdf25534562a0c8f5dff8f265486304926b
Louis <louis.mueller@axonivy.com>
2024-07-25 13:53:17 +0200

    XIVY-14547 add process animation test

---
+++ playwright/tests/process-animation.test.ts
@@ -0,0 +1,17 @@
+import { test } from './fixtures/page';
+import { ProcessEditor } from './page-objects/process-editor';
+import { prebuiltWorkspacePath } from './workspaces/workspace';
+
+test.describe('Process Animation', () => {
+  test('Animate Call Sub', async ({ pageFor }) => {
+    const page = await pageFor(prebuiltWorkspacePath);
+    const processEditor = new ProcessEditor(page, 'Animation.p.json');
+    await processEditor.hasDeployProjectStatusMessage();
+    await processEditor.openEditorFile();
+    await processEditor.isViewVisible();
+
+    const start = processEditor.locatorForPID('190E9B4311474684-f0');
+    const scriptInCallSub = processEditor.locatorForPID('190E938617AE0413-f3');
+    await processEditor.startProcessAndAssertExecuted(start, scriptInCallSub);
+  });
+});
