0da48cecb5313b86755b8bf3083fadb65d7cb80a
Louis <louis.mueller@axonivy.com>
2024-07-26 08:30:15 +0200

    wip

--- playwright/tests/process-animation.test.ts
+++ playwright/tests/process-animation.test.ts
@@ -1,27 +1,18 @@
+import { test } from './fixtures/page';
 import { ProcessEditor } from './page-objects/process-editor';
 import { prebuiltWorkspacePath } from './workspaces/workspace';
-import { pageFor } from './fixtures/page';
-import { expect, test } from '@playwright/test';
 
 test.describe('Process Animation', () => {
-  let processEditor: ProcessEditor;
-
-  test.beforeAll(async ({}, testInfo) => {
-    const page = await pageFor(prebuiltWorkspacePath, testInfo.titlePath[1]);
-    processEditor = new ProcessEditor(page, 'Animation.p.json');
+  test('Animate Call Sub', async ({ pageFor }) => {
+    const page = await pageFor(prebuiltWorkspacePath);
+    const processEditor = new ProcessEditor(page, 'Animation.p.json');
     await processEditor.hasDeployProjectStatusMessage();
-  });
-
-  test.beforeEach(async () => {
+    await processEditor.hasNoStatusMessage();
     await processEditor.openEditorFile();
     await processEditor.isViewVisible();
-  });
 
-  test('Animate Call Sub', async () => {
     const start = processEditor.locatorForPID('190E9B4311474684-f0');
     const scriptInCallSub = processEditor.locatorForPID('190E938617AE0413-f3');
-    await processEditor.page.waitForTimeout(1000);
-    await expect(start).toBeVisible();
     await processEditor.startProcessAndAssertExecuted(start, scriptInCallSub);
   });
 });
