f04e0a8316a53e2789f071c2182781c7fa8a936c
Louis <louis.mueller@axonivy.com>
2024-08-30 10:53:15 +0200

    fix animation 2

--- playwright/tests/process-animation.test.ts
+++ playwright/tests/process-animation.test.ts
@@ -1,34 +1,41 @@
-import { expect, Locator } from '@playwright/test';
-import { test } from './fixtures/page';
+import { test, expect, Page } from '@playwright/test';
+import { pageFor } from './fixtures/page';
 import { ProcessEditor } from './page-objects/process-editor';
 import { animationWorkspacePath } from './workspaces/workspace';
+import { FileExplorer } from './page-objects/explorer-view';
 
 test.describe('Process Animation', () => {
-  let processEditor: ProcessEditor;
-  let start: Locator;
+  let page: Page;
 
-  test.beforeEach(async ({ pageFor }) => {
-    const page = await pageFor(animationWorkspacePath);
-    processEditor = new ProcessEditor(page, 'Animation.p.json');
-    start = processEditor.locatorForPID('190EEC366DECC66A-f0');
-    await processEditor.openEditorFile();
-    await processEditor.isViewVisible();
-    await processEditor.hasDeployProjectStatusMessage();
+  test.beforeAll(async ({}, testInfo) => {
+    page = await pageFor(animationWorkspacePath, testInfo.titlePath[1]);
+    await new FileExplorer(page).hasDeployProjectStatusMessage();
   });
 
   test('with activated animation', async () => {
+    const processEditor = new ProcessEditor(page, 'Animation.p.json');
+    await processEditor.openEditorFile();
+    const start = processEditor.locatorForPID('190EEC366DECC66A-f0');
+    await expect(start).toBeVisible();
+
     await processEditor.executeCommand('Axon Ivy: Activate Process Animation');
     await processEditor.page.waitForTimeout(2_000); // ensure config is respected
-    const scriptInCallSub = processEditor.locatorForPID('190EEC3ABECE2C88-f3');
-    await processEditor.startProcessAndAssertExecuted(start, scriptInCallSub);
+    const taskInCallSub = processEditor.locatorForPID('190EEC3ABECE2C88-f5');
+    await processEditor.startProcessAndAssertExecuted(start, taskInCallSub);
+    await processEditor.page.waitForTimeout(500); //ensure animation finished
   });
 
   test('with deactivated animation', async () => {
+    const processEditor = new ProcessEditor(page, 'NoAnimation.p.json');
+    await processEditor.openEditorFile();
+    const start = processEditor.locatorForPID('191A2645F90CDC61-f0');
+    await expect(start).toBeVisible();
+
     await processEditor.executeCommand('Axon Ivy: Deactivate Process Animation');
     await processEditor.page.waitForTimeout(2_000); // ensure config is respected
-    const callSub = processEditor.locatorForPID('190EEC366DECC66A-f3');
+    const callSub = processEditor.locatorForPID('191A2645F90CDC61-f3');
     await processEditor.startProcessAndAssertExecuted(start, callSub);
-    await processEditor.page.waitForTimeout(1_00); // to ensure no jump
+    await processEditor.page.waitForTimeout(1_000); // to ensure no jump
     await expect(callSub).toBeVisible();
   });
 });
