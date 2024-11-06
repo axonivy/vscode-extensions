a6ea2d109c068ac6e943c083dd8b4a3dbb1f7fc7
Lukas Lieb <lukas.lieb@axonivy.com>
2024-11-06 09:01:23 +0100

    XIVY-15282 Catch openUrl action and open url in internal browser

--- extension/src/editors/config-editor/webview-communication.ts
+++ extension/src/editors/config-editor/webview-communication.ts
@@ -3,6 +3,8 @@ import { Messenger } from 'vscode-messenger';
 import { DisposableCollection } from '@eclipse-glsp/vscode-integration';
 import { MessageParticipant, NotificationType } from 'vscode-messenger-common';
 import { WebSocketForwarder } from '../websocket-forwarder';
+import type { VariablesActionArgs } from '@axonivy/variable-editor-protocol';
+import { IvyBrowserViewProvider } from '../../browser/ivy-browser-view-provider';
 
 const WebviewReadyNotification: NotificationType<void> = { method: 'ready' };
 const InitializeConnectionRequest: NotificationType<{ file: string }> = { method: 'initializeConnection' };
@@ -27,7 +29,12 @@ export const setupCommunication = (
 };
 
 class VariableEditorWebSocketForwarder extends WebSocketForwarder {
-  constructor(websocketUrl: URL, messenger: Messenger, messageParticipant: MessageParticipant, readonly document: vscode.TextDocument) {
+  constructor(
+    websocketUrl: URL,
+    messenger: Messenger,
+    messageParticipant: MessageParticipant,
+    readonly document: vscode.TextDocument
+  ) {
     super(websocketUrl, 'ivy-config-lsp', messenger, messageParticipant, ConfigWebSocketMessage);
   }
 
@@ -35,11 +42,21 @@ class VariableEditorWebSocketForwarder extends WebSocketForwarder {
     if (this.isSaveData(message)) {
       this.writeTextDocument(message.params.data);
     }
+    if (this.isAction(message)) {
+      if (message.params.actionId === 'openUrl') {
+        IvyBrowserViewProvider.instance.open(message.params.payload);
+      }
+      return;
+    }
     super.handleClientMessage(message);
   }
 
   isSaveData = (obj: unknown): obj is { jsonrpc: string; id: number; method: string; params: { data: string } } => {
     return typeof obj === 'object' && obj !== null && 'method' in obj && obj.method === 'saveData';
+  };
+
+  isAction = (obj: unknown): obj is { method: string; params: VariablesActionArgs } => {
+    return typeof obj === 'object' && obj !== null && 'method' in obj && obj.method === 'action';
   };
 
   writeTextDocument = (content: string) => {
