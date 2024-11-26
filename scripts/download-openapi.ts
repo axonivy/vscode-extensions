98c15cf30d60ee304b2e5090e8fba7d7b154f046
Louis <louis.mueller@axonivy.com>
2024-11-26 10:55:34 +0100

    raise orval to 7.3 and node to 22.11

--- scripts/download-openapi.ts
+++ scripts/download-openapi.ts
@@ -3,9 +3,9 @@ const { Readable } = require('stream');
 import { finished } from 'stream/promises';
 
 const downloadOpenapi = async () => {
-  const url = process.argv[2] ? new URL(process.argv[2]) : new URL('https://neo.demo.ivyteam.io');
+  const url = process.argv[2] ? new URL(process.argv[2]) : new URL('https://neo.ivyteam.io');
   await download('openapi-default.yaml', new URL('api/openapi.yaml', url));
-  await download('openapi-dev.yaml', new URL('ivy-dev-workflow-demos/api/openapi.yaml', url));
+  await download('openapi-dev.yaml', new URL('~Developer-workflow-demos/api/openapi.yaml', url));
 };
 
 const download = async (fileName: string, url: URL) => {
