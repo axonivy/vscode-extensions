80ef4409649b3464156aaa05889635decca79b40
Louis <louis.mueller@axonivy.com>
2025-01-30 16:53:58 +0100

    w

--- playwright/tests/page-objects/page-object.ts
+++ playwright/tests/page-objects/page-object.ts
@@ -6,14 +6,12 @@ export class PageObject {
   async executeCommand(command: string, ...userInputs: Array<string>) {
     await expect(this.page.locator('div.command-center')).toBeAttached();
     const quickInputList = this.page.locator('.quick-input-list');
-    await expect(async () => {
-      await this.page.keyboard.press('F1');
-      await this.page.waitForTimeout(100);
-      await this.quickInputBox()
-        .locator('input.input')
-        .fill('>' + command, { timeout: 100 });
-      await this.page.keyboard.press('Enter');
-    }).toPass();
+    await this.page.keyboard.press('F1');
+    await this.page.waitForTimeout(100);
+    await this.quickInputBox()
+      .locator('input.input')
+      .fill('>' + command);
+    await this.page.keyboard.press('Enter');
     for (const userInput of userInputs) {
       this.provideUserInput(userInput);
     }
