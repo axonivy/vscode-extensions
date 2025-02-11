b5c3dd068d54d1d94fbb6941de725ee15540eda0
Louis <louis.mueller@axonivy.com>
2025-02-11 12:23:14 +0100

    rename workspaces

--- playwright/tests/workspaces/workspace.ts
+++ playwright/tests/workspaces/workspace.ts
@@ -5,11 +5,11 @@ import { randomInt } from 'crypto';
 export const animationWorkspacePath = path.resolve(__dirname, './animationProject');
 export const prebuiltWorkspacePath = path.resolve(__dirname, './prebuiltProject');
 export const prebuiltEmptyWorkspacePath = path.resolve(__dirname, './prebuiltEmptyProject');
-export const noEngineWorkspacePath = path.resolve(__dirname, './no-engine');
-export const noProjectWorkspacePath = path.resolve(__dirname, './no-project');
-export const multiProjectWorkspacePath = path.resolve(__dirname, './multi-project');
+export const noEngineWorkspacePath = path.resolve(__dirname, './noEngine');
+export const noProjectWorkspacePath = path.resolve(__dirname, './noProject');
+export const multiProjectWorkspacePath = path.resolve(__dirname, './multiProject');
 export const empty = path.resolve(__dirname, './empty');
-export const embeddedEngineWorkspace = path.resolve(__dirname, './embedded-engine');
+export const embeddedEngineWorkspace = path.resolve(__dirname, './embeddedEngine');
 
 export function removeFromWorkspace(...paths: string[]) {
   if (process.env.CI) {
