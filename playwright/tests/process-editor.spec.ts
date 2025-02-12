95af041db54aa09936605aaa841aa0293e94b727
Louis <louis.mueller@axonivy.com>
2025-02-12 15:53:27 +0100

    s

--- playwright/tests/process-editor.spec.ts
+++ playwright/tests/process-editor.spec.ts
@@ -8,7 +8,7 @@ import { getCtrlOrMeta } from './utils/keyboard';
 const userDialogPID = '15254DCE818AD7A2-f3';
 
 // eslint-disable-next-line playwright/no-focused-test
-test.only('Process Editor', () => {
+test.describe.only('Process Editor', () => {
   let processEditor: ProcessEditor;
   let page: Page;
 
