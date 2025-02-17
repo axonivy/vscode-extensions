3ea4d05f81b6d184bf38f6ab542b80a8e7e8d404
Louis <louis.mueller@axonivy.com>
2025-02-17 17:19:12 +0100

    make fail

--- playwright/tests/browser-view.spec.ts
+++ playwright/tests/browser-view.spec.ts
@@ -1,6 +1,6 @@
 import { test } from './fixtures/baseTest';
 
 test('Open Engine Cockpit', async ({ page }) => {
-  await page.getByRole('treeitem', { name: 'pom.xml', exact: true }).locator('a').click();
+  await page.getByRole('treeitem', { name: 'pom.xml', exact: true }).locator('axxx').click();
   console.log('Hello');
 });
