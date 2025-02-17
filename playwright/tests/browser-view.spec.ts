f03cfa1f2c527f46a6335b4813ce6288f8f6eddf
Louis <louis.mueller@axonivy.com>
2025-02-17 17:12:22 +0100

    new fixture

--- playwright/tests/browser-view.spec.ts
+++ playwright/tests/browser-view.spec.ts
@@ -1,56 +1,6 @@
-import { Page, expect, test } from '@playwright/test';
-import { pageFor } from './fixtures/page';
-import { prebuiltWorkspacePath } from './workspaces/workspace';
-import { BrowserView } from './page-objects/browser-view';
+import { test } from './fixtures/baseTest';
 
-test.describe('Browser View', () => {
-  let page: Page;
-  let browserView: BrowserView;
-
-  test.beforeAll(async ({}, testInfo) => {
-    page = await pageFor(prebuiltWorkspacePath, testInfo.titlePath[1]);
-    browserView = new BrowserView(page);
-    await browserView.hasDeployProjectStatusMessage();
-  });
-
-  test('Toolbar', async () => {
-    const home = /home.xhtml/;
-    const starts = /starts.xhtml/;
-    await browserView.openDevWfUi();
-    await assertToolbarInput(/dev-workflow-ui/);
-
-    await browserView.home().click();
-    await assertToolbarInput(home);
-
-    const startLink = browserView.content().locator('#menuform\\:sr_starts');
-    await expect(startLink).toBeVisible();
-    await browserView.content().locator('#menuform\\:sr_starts').click();
-    await assertToolbarInput(starts);
-
-    await browserView.reload().click({ clickCount: 2 });
-    await assertToolbarInput(starts);
-
-    await browserView.back().click();
-    await assertToolbarInput(starts);
-
-    await browserView.back().click();
-    await assertToolbarInput(home);
-
-    await browserView.forward().click();
-    await assertToolbarInput(starts);
-  });
-
-  test('Open Engine Cockpit', async () => {
-    await browserView.openCockpit();
-    await assertToolbarInput(/system\/engine-cockpit/);
-  });
-
-  test('Open NEO', async () => {
-    await browserView.openNEO();
-    await assertToolbarInput(/neo/);
-  });
-
-  async function assertToolbarInput(value: RegExp) {
-    await expect(browserView.input()).toHaveValue(value);
-  }
+test('Open Engine Cockpit', async ({ page }) => {
+  await page.getByRole('treeitem', { name: 'pom.xml', exact: true }).locator('a').click();
+  console.log('Hello');
 });
