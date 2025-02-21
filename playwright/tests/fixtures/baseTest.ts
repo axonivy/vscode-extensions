c9151c09f498ce539fbb3b1462a5e086cf8b8a2a
Louis <louis.mueller@axonivy.com>
2025-02-21 11:07:20 +0100

    make fixture less strange

--- playwright/tests/fixtures/baseTest.ts
+++ playwright/tests/fixtures/baseTest.ts
@@ -1,4 +1,4 @@
-import { test as base, _electron, Page, chromium, Browser, ElectronApplication } from '@playwright/test';
+import { test as base, _electron, Page, chromium } from '@playwright/test';
 import { downloadAndUnzipVSCode } from '@vscode/test-electron/out/download';
 export { expect } from '@playwright/test';
 import path from 'path';
@@ -11,44 +11,22 @@ export const runInBrowser = process.env.RUN_IN_BRWOSER ? true : false;
 export const test = base.extend<{ workspace: string; page: Page }>({
   workspace: prebuiltWorkspacePath,
   page: async ({ workspace }, take) => {
-    const { page, browser, electronApp } = await appWithPage(workspace);
-    await initialize(page);
-    await take(page);
-    if (browser) {
-      await browser.close();
-      return;
-    }
-    if (electronApp) {
-      if (test.info().status === 'failed') {
-        const tracePath = test.info().outputPath('trace.zip');
-        const screenshotPath = test.info().outputPath('screenshot.png');
-        await page.context().tracing.stop({ path: tracePath });
-        await page.screenshot({ path: screenshotPath });
-        test.info().attachments.push({ name: 'trace', path: tracePath, contentType: 'application/zip' });
-        test.info().attachments.push({ name: 'screenshot', path: tracePath, contentType: 'image/png' });
-      }
-      await electronApp.close();
-    }
+    if (runInBrowser) await runBrowserTest(workspace, take);
+    else await runElectronAppTest(workspace, take);
   }
 });
 
-const appWithPage = async (workspace: string): Promise<{ page: Page; browser?: Browser; electronApp?: ElectronApplication }> => {
-  if (runInBrowser) {
-    return browser(workspace);
-  }
-  return electron(workspace);
-};
-
-const browser = async (workspace: string) => {
+const runBrowserTest = async (workspace: string, take: (r: Page) => Promise<void>) => {
   const browser = await chromium.launch();
   const page = await browser.newPage();
   await page.setViewportSize({ width: 1920, height: 1080 });
   await page.goto(`http://localhost:3000/?folder=/home/workspace/${workspace.split('/tests/workspaces/')[1]}`);
   await initialize(page);
-  return { page, browser };
+  await take(page);
+  await browser.close();
 };
 
-const electron = async (workspace: string) => {
+const runElectronAppTest = async (workspace: string, take: (r: Page) => Promise<void>) => {
   const vscodePath = await downloadAndUnzipVSCode(downloadVersion);
   const electronApp = await _electron.launch({
     executablePath: vscodePath,
@@ -65,7 +43,17 @@ const electron = async (workspace: string) => {
   });
   const page = await electronApp.firstWindow();
   await page.context().tracing.start({ screenshots: true, snapshots: true, title: test.info().title });
-  return { page, electronApp };
+  await initialize(page);
+  await take(page);
+  if (test.info().status === 'failed') {
+    const tracePath = test.info().outputPath('trace.zip');
+    const screenshotPath = test.info().outputPath('screenshot.png');
+    await page.context().tracing.stop({ path: tracePath });
+    await page.screenshot({ path: screenshotPath });
+    test.info().attachments.push({ name: 'trace', path: tracePath, contentType: 'application/zip' });
+    test.info().attachments.push({ name: 'screenshot', path: tracePath, contentType: 'image/png' });
+  }
+  await electronApp.close();
 };
 
 const initialize = async (page: Page) => {
