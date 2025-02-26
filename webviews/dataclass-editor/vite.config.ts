05c0e391ae69a8d478c06767f4579c7e02b1db44
Louis <louis.mueller@axonivy.com>
2025-02-26 08:39:53 +0100

    Update webviews/dataclass-editor/vite.config.ts
    Co-authored-by: Lukas Lieb <42733123+ivy-lli@users.noreply.github.com>

--- webviews/dataclass-editor/vite.config.ts
+++ webviews/dataclass-editor/vite.config.ts
@@ -1,15 +1,12 @@
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig(() => {
-  const config = {
-    plugins: [tsconfigPaths()],
-    build: {
-      manifest: 'build.manifest.json',
-      outDir: '../../extension/dist/webviews/dataclass-editor',
-      chunkSizeWarningLimit: 5000
-    },
-    base: './'
-  };
-  return config;
-});
+export default defineConfig(() => ({
+  plugins: [tsconfigPaths()],
+  build: {
+    manifest: 'build.manifest.json',
+    outDir: '../../extension/dist/webviews/dataclass-editor',
+    chunkSizeWarningLimit: 5000
+  },
+  base: './'
+}));
