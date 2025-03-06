55e672ac14335a9542f864e637d24beca3e77b37
Louis <louis.mueller@axonivy.com>
2025-03-06 11:00:58 +0100

    disable theme provider (#452)

--- webviews/dataclass-editor/src/index.tsx
+++ webviews/dataclass-editor/src/index.tsx
@@ -5,6 +5,7 @@ import { Messenger, VsCodeApi } from 'vscode-messenger-webview';
 import { InitializeConnection, initMessenger, toConnection } from 'vscode-webview-common';
 import 'vscode-webview-common/css/colors.css';
 import '@axonivy/dataclass-editor/lib/editor.css';
+import { ThemeProvider } from '@axonivy/ui-components';
 
 declare function acquireVsCodeApi(): VsCodeApi;
 const messenger = new Messenger(acquireVsCodeApi());
@@ -19,11 +20,13 @@ export async function start({ file }: InitializeConnection) {
   }
   createRoot(rootElement).render(
     <React.StrictMode>
-      <ClientContextProvider client={client}>
-        <QueryProvider client={queryClient}>
-          <DataClassEditor context={{ app: '', pmv: '', file }} />
-        </QueryProvider>
-      </ClientContextProvider>
+      <ThemeProvider disabled={true}>
+        <ClientContextProvider client={client}>
+          <QueryProvider client={queryClient}>
+            <DataClassEditor context={{ app: '', pmv: '', file }} />
+          </QueryProvider>
+        </ClientContextProvider>
+      </ThemeProvider>
     </React.StrictMode>
   );
 }
