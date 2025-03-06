67d79b720ef4e3aaf1e65f25a977625a4f328335
Louis <louis.mueller@axonivy.com>
2025-03-06 12:22:40 +0100

    github repoter

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -12,5 +12,5 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   timeout: 60_000,
   expect: { timeout: 30_000 },
-  reporter: process.env.CI ? [['junit', { outputFile: 'report.xml' }]] : 'html'
+  reporter: 'github'
 });
