e54ffea8a48845ee99aac7c6182fb2904baf1517
Louis <louis.mueller@axonivy.com>
2025-03-06 10:13:56 +0100

    disable theme provider

--- webviews/form-editor/src/index.tsx
+++ webviews/form-editor/src/index.tsx
@@ -7,6 +7,7 @@ import { createRoot } from 'react-dom/client';
 import { InitializeConnection, initMessenger, toConnection } from 'vscode-webview-common';
 import { VsCodeApi, Messenger } from 'vscode-messenger-webview';
 import '@axonivy/form-editor/lib/editor.css';
+import { ThemeProvider } from '@axonivy/ui-components';
 
 declare function acquireVsCodeApi(): VsCodeApi;
 const messenger = new Messenger(acquireVsCodeApi());
@@ -22,11 +23,13 @@ export async function start({ file }: InitializeConnection): Promise<void> {
   }
   createRoot(rootElement).render(
     <React.StrictMode>
-      <ClientContextProvider client={client}>
-        <QueryProvider client={queryClient}>
-          <App context={{ app: '', pmv: '', file }} />
-        </QueryProvider>
-      </ClientContextProvider>
+      <ThemeProvider disabled={true}>
+        <ClientContextProvider client={client}>
+          <QueryProvider client={queryClient}>
+            <App context={{ app: '', pmv: '', file }} />
+          </QueryProvider>
+        </ClientContextProvider>
+      </ThemeProvider>
     </React.StrictMode>
   );
 }
