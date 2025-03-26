c3b2d8b515b82edce61a667eaac55b753552821c
Lukas Giger <lukas.giger@axonivy.com>
2025-03-26 15:17:43 +0100

    make open cms command use selected project

--- extension/src/editors/cms-editor/open-cms-editor-cmd.ts
+++ extension/src/editors/cms-editor/open-cms-editor-cmd.ts
@@ -1,13 +1,30 @@
 import * as vscode from 'vscode';
 import { messenger } from '../..';
 import { registerCommand } from '../../base/commands';
+import { IvyProjectExplorer } from '../../project-explorer/ivy-project-explorer';
+import { TreeSelection, treeSelectionToProjectPath } from '../../project-explorer/tree-selection';
 import { createWebViewContent } from '../webview-helper';
 import { setupCommunication } from './webview-communication';
 
 export const registerOpenCmsEditorCmd = (context: vscode.ExtensionContext, websocketUrl: URL) => {
-  registerCommand('ivyBrowserView.openCmsEditor', context, () => {
+  registerCommand('ivyBrowserView.openCmsEditor', context, async (selection: TreeSelection) => {
+    let projectPath: string | undefined;
+    try {
+      projectPath = await treeSelectionToProjectPath(selection, IvyProjectExplorer.instance.getIvyProjects());
+    } catch (error) {
+      showError(error instanceof Error ? error.message : String(error));
+      return;
+    }
+    if (!projectPath) {
+      showError('No valid Axon Ivy Project selected.');
+      return;
+    }
     const webviewPanel = vscode.window.createWebviewPanel('cms-editor', 'CMS', vscode.ViewColumn.One, { enableScripts: true });
-    setupCommunication(websocketUrl, messenger, webviewPanel);
+    setupCommunication(websocketUrl, messenger, webviewPanel, projectPath);
     webviewPanel.webview.html = createWebViewContent(context, webviewPanel.webview, 'cms-editor');
   });
+};
+
+const showError = (message: string) => {
+  vscode.window.showErrorMessage(`Open CMS Editor: ${message}`);
 };
