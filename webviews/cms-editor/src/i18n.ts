4f41208fd8b0d5e94ea3af6b952bb2383ca272aa
Fabian Heuberger <fabian.heuberger@axonivy.com>
2025-04-09 11:43:11 +0200

    XIVY-16283 removes_common_namespaces

--- webviews/cms-editor/src/i18n.ts
+++ webviews/cms-editor/src/i18n.ts
@@ -1,17 +1,15 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import { enTranslation, enCommonTranslation } from '@axonivy/cms-editor';
+import { enTranslation } from '@axonivy/cms-editor';
 
 export const initTranslation = () => {
   if (i18n.isInitializing || i18n.isInitialized) return;
-  i18n
-    .use(initReactI18next)
-    .init({
-      debug: true,
-      supportedLngs: ['en'],
-      fallbackLng: 'en',
-      ns: ['cms-editor'],
-      defaultNS: 'cms-editor',
-      resources: { en: { 'cms-editor': enTranslation, common: enCommonTranslation } }
-    });
+  i18n.use(initReactI18next).init({
+    debug: true,
+    supportedLngs: ['en'],
+    fallbackLng: 'en',
+    ns: ['cms-editor'],
+    defaultNS: 'cms-editor',
+    resources: { en: { 'cms-editor': enTranslation } }
+  });
 };
--- webviews/dataclass-editor/src/i18n.ts
+++ webviews/dataclass-editor/src/i18n.ts
@@ -1,17 +1,15 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import { enTranslation, enCommonTranslation } from '@axonivy/dataclass-editor';
+import { enTranslation } from '@axonivy/dataclass-editor';
 
 export const initTranslation = () => {
   if (i18n.isInitializing || i18n.isInitialized) return;
-  i18n
-    .use(initReactI18next)
-    .init({
-      debug: true,
-      supportedLngs: ['en'],
-      fallbackLng: 'en',
-      ns: ['dataclass-editor'],
-      defaultNS: 'dataclass-editor',
-      resources: { en: { 'dataclass-editor': enTranslation, common: enCommonTranslation } }
-    });
+  i18n.use(initReactI18next).init({
+    debug: true,
+    supportedLngs: ['en'],
+    fallbackLng: 'en',
+    ns: ['dataclass-editor'],
+    defaultNS: 'dataclass-editor',
+    resources: { en: { 'dataclass-editor': enTranslation } }
+  });
 };
