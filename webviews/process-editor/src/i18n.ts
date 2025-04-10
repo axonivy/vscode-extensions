67fe1a9648ecb867c2f88eaf41ac3c752e3bd531
Fabian Heuberger <fabian.heuberger@axonivy.com>
2025-04-10 13:04:47 +0200

    XIVY-16283 removes_common_namespaces

--- webviews/process-editor/src/i18n.ts
+++ webviews/process-editor/src/i18n.ts
@@ -1,6 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import { enTranslation, enCommonTranslation } from '@axonivy/process-editor';
+import { enTranslation } from '@axonivy/process-editor';
 
 export const initTranslation = () => {
   if (i18n.isInitializing || i18n.isInitialized) return;
@@ -10,6 +10,6 @@ export const initTranslation = () => {
     fallbackLng: 'en',
     ns: ['process-editor'],
     defaultNS: 'process-editor',
-    resources: { en: { 'process-editor': enTranslation, common: enCommonTranslation } }
+    resources: { en: { 'process-editor': enTranslation } }
   });
 };
